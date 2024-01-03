0ada7f054f4eccca984d7f70149c2fd8111ed9f1
CN\Mace Y Li <mace.y.li@cn.pwc.com>
2024-01-03 17:06:47 +0800

    worker完成

--- core/worker.js
+++ core/worker.js
@@ -1,6 +1,6 @@
-
+/* 
 self.onmessage = function(e){
-    let {interval,checkSiteHost,intervalAddTime,maxInterval,isStop,isCanRun,domSccripts,domCsss,checkWho} = e.data,
+    let {checkSiteHost,domSccripts,domCsss,checkWho} = e.data,
     htmlText = '',
     errText = '';
 
@@ -62,34 +62,142 @@ self.onmessage = function(e){
 
     
     getWebSite.then(res=>{
-        let siteObj = extractLinksAndScripts(htmlText);
+        let siteObj = extractLinksAndScripts(htmlText),
+        diffResult = null;
         if(checkWho.includes('script')){
-            
+            diffResult = differentValue(domSccripts,siteObj._script);
+            if(diffResult !== null){
+                self.postMessage({
+                    update:1,
+                    msg:'script_diff'
+                })
+                return;
+            }
         }
 
         if(checkWho.includes('css')){
-
+            diffResult = differentValue(domCsss,siteObj._css);
+            if(diffResult !== null){
+                self.postMessage({
+                    update:1,
+                    msg:'css_diff'
+                })
+                return;
+            }
         }
+
+        self.postMessage({
+            update:0,
+            msg:'no_file_update'
+        })
     }).catch(err=>{
-        
+        self.postMessage({
+            update:-1,
+            msg:'no_file_update'
+        })
     })
-      
-}
+} */
 
 function getWorkerBlob(){
     let js = `
-        self.onmessage = function(e){
-            let {interval,checkSiteHost,intervalAddTime,maxInterval} = e.data;
+    self.onmessage = function(e){
+        let {checkSiteHost,domSccripts,domCsss,checkWho} = e.data,
+        htmlText = '',
+        errText = '';
+    
+    
+        function getWebSite(){
+            return new Promise((r,rj)=>{
+                fetch(checkSiteHost).then(res=>{
+                    return res.text();
+                }).then(_htmlText =>{
+                    htmlText = _htmlText;
+                    r('ok')
+                }).catch(err=>{
+                    errText = err;
+                    console.warn('get web site error:',errText)
+                    rj('wrong')
+                })
+            })
+        }
+    
+        function extractLinksAndScripts(str) {
+            const regexLink = /<link[^>]+href=['"]([^'"]+\.(css))['"][^>]*>/g;
+            const regexLinkJs = /<link[^>]+href=['"]([^'"]+\.(js))['"][^>]*>/g;
+            const regexScript = /<script[^>]+src=['"]([^'"]+\.(js))['"][^>]*><\/script>/g;
+            const result = {
+              _css: [],
+              _script: []
+            };
+          
+            let match;
+            while ((match = regexLink.exec(str))) {
+              result._css.push(match[1]);
+            }
+          
+            while ((match = regexScript.exec(str))) {
+              result._script.push(match[1]);
+            }
             
+            while ((match = regexLinkJs.exec(str))) {
+              result._script.push(match[1]);
+            }
+          
+            return result;
         }
-
-        self.onerror = function(e){
-            console.error('webDetectorWorker worker error:',e);
+    
+    
+    
+        function differentValue(arr1, arr2) {
+            var targetArray = arr1.length <= arr2.length ? arr1 : arr2;
+            var otherArray = targetArray === arr1 ? arr2 : arr1;
+          
+            for (var i = 0; i < targetArray.length; i++) {
+              if (targetArray[i] !== otherArray[i]) {
+                return targetArray[i];
+              }
+            }
+          
+            return null;
         }
-
-        self.onmessageerror = function(e){
-            console.error('webDetectorWorker woker onmessageerror:',e);
-        }
+    
+        
+        getWebSite.then(res=>{
+            let siteObj = extractLinksAndScripts(htmlText),
+            diffResult = null;
+            if(checkWho.includes('script')){
+                diffResult = differentValue(domSccripts,siteObj._script);
+                if(diffResult !== null){
+                    self.postMessage({
+                        update:1,
+                        msg:'script_diff'
+                    })
+                    return;
+                }
+            }
+    
+            if(checkWho.includes('css')){
+                diffResult = differentValue(domCsss,siteObj._css);
+                if(diffResult !== null){
+                    self.postMessage({
+                        update:1,
+                        msg:'css_diff'
+                    })
+                    return;
+                }
+            }
+    
+            self.postMessage({
+                update:0,
+                msg:'no_file_update'
+            })
+        }).catch(err=>{
+            self.postMessage({
+                update:-1,
+                msg:'no_file_update'
+            })
+        })
+    }
     `,
     _blob = new Blob([js]);
 
