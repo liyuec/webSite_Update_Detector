fc7544bcefad7ece2e5050ed68644b715218477d
CN\Mace Y Li <mace.y.li@cn.pwc.com>
2024-01-08 13:58:39 +0800

    update:构建文件测试 + yalc

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,28 +1,30 @@
 import {terser} from "rollup-plugin-minification";
 import resolve from '@rollup/plugin-node-resolve';
-import vue from 'rollup-plugin-vue';
-import postcss from 'rollup-plugin-postcss';
-import autoprefixer from 'autoprefixer';
 import common from 'rollup-plugin-commonjs';
 import babel from '@rollup/plugin-babel';
+import clearDirectory from 'rollup-plugin-clear-directory';
 
-
+console.log('****************rollup.config.js******************')
 export default{
-    input:'../../index.js',
+    input:'index.js',
     output:[
         {
-            file:'../dist/index.esm.js',
+            file:'./dist/index.esm.js',
             format:'esm',
             //sourcemap:true
             exports:'named'
         },
         {
-            file:'../dist/index.umd.js',
+            file:'./dist/index.umd.js',
             format:'umd',
             name:'detectorumd'
         }
     ],
     plugins:[
+        clearDirectory({
+            targets: ['dist']
+          }
+        ),
         common(),
         resolve(),
         terser()
--- build/rollup.link.config.js
+++ build/rollup.link.config.js
@@ -1,23 +1,28 @@
 import resolve from '@rollup/plugin-node-resolve';
 import common from 'rollup-plugin-commonjs';
+import clearDirectory from 'rollup-plugin-clear-directory';
 
 console.log('****************rollup.link.config.js******************')
 export default{
-    input:'../../index.js',
+    input:'index.js',
     output:[
         {
-            file:'../dist/index.esm.js',
+            file:'./dist/index.esm.js',
             format:'esm',
             //sourcemap:true
             exports:'named'
         },
         {
-            file:'../dist/index.umd.js',
+            file:'./dist/index.umd.js',
             format:'umd',
             name:'detectorumd'
         }
     ],
     plugins:[
+        clearDirectory({
+            targets: ['dist']
+          }
+        ),
         common(),
         resolve()
     ]
